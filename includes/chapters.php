3ec2dc1759e6ffe60cf385980a6b2e50f9563a8a
Simon <simon.sterner@etu.se>
2026-03-24 09:54:54 +0100

    Added chapters.php array with all the names of the DIGG-named criterions that align with our working sheet.

---
+++ includes/chapters.php
@@ -0,0 +1,126 @@
+<?php
+
+$chapters = [
+    'Störande' => [
+        'Ljudkontroll',
+        'Rörligt innehåll',
+        'Flimmer',
+    ],
+
+    'Innehåll' => [
+        'Sidtitel',
+        'Sidans språk',
+        'Automatisk omladdning',
+        'Semantik' => [
+            'Semantik',
+            'Landmärken',
+            'Rubriker',
+            'Listor',
+            'Tabeller',
+        ],
+        'Länkar' => [
+            'Syfte för länkar',
+            'Färganvändning för länkar',
+            'Ikon-/symbollänkar',
+        ],
+        'Text' => [
+            'Språkändringar',
+            'Rubriker',
+            'Kontrast för text',
+            'Sensoriska hänvisningar',
+        ],
+    ],
+    'Tangentbord' => [
+        'Fokus' => [
+            'Fokusordning',
+            'Synlig fokusmarkering',
+            'Kontrast för fokusmarkering',
+            'Kontextförändring vid fokus',
+            'Uppdykande innehåll vid fokus',
+        ],
+        'Hanterbart' => [
+            'Skipplänkar',
+            'Hanterbart med tangentbord',
+            'Ingen tangentbordsfälla',
+        ]
+    ],
+
+    'Layout' => [
+        'Flexibel layout',
+        'Utöka textavstånd',
+        'Förstoring',
+        'Användning av färg',
+    ],
+
+    'Generella krav' => [
+        'Statusmeddelanden',
+        'Tidsgränser',
+        'Innehållets ordning',
+        'Konsekvent navigering',
+        'Konsekvent benämning',
+        'Flera sätt att hitta sidan',
+        'Avbryta klick',
+        'Användarinställningar',
+    ],
+
+    'Avvikande sidor' => [
+        'Förhindra allvarliga konsekvenser',
+        'En-knapps snabbtangenter',
+        'Aktivering av tillgänglighetsfunktioner',
+        'Biometri',
+        'Bevara tillgänglighet vid omvandling',
+    ],
+
+    'Bilder' => [
+        'Kontrast i grafik',
+        'Bilder av text',
+        'Kontrast för bilder av text',
+        'Textalternativ' => [
+            'Textalternativ för bilder',
+            'Kontrast för textalternativ',
+            'Språk för textalternativ',
+        ]
+    ],
+
+    'Användargränssnitt' => [
+        'Ledtexter' => [
+            'Ledtexter, instruktioner',
+            'Kopplade ledtexter',
+            'Begripliga ledtexter', 
+        ],
+        'Interaktiva komponenter' => [
+            'Kopplade fält',
+            'Namn, roll, värde',
+            'Etiketter i namn',
+            'Ikon-/symbolknappar',
+            'Kontrast för komponenter',
+            'Syfte för inmatningsfält',
+        ],
+        'Beteende' => [
+            'Uppdykande innehåll vid hovring',
+            'Kontextförändring vid inmatning',
+        ],
+        
+        'Felmeddelanden' => [
+            'Felmeddelanden',
+            'Färganvändning för felmeddelanden',
+            'Korrigeringsförslag',
+        ],
+    ],
+
+    'Ljud och video' => [
+        	'Textalternativ för tidsberoende medier',
+            'Alternativ för ljudklipp',
+            'Alternativ för animeringar och filmer',
+            'Reglage',
+            'Undertexter' => [
+                'Undertexter',
+            ],
+            'Syntolkning' => [
+                'Syntolkning',
+                'Syntolkning eller transkription',
+            ]
+    ],
+]
+
+?>
